f4acece26f215dfa2c84d96d6f707fead1be8f3b
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2025-07-04 04:35:21 +0700

    update structure

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('../routes/auth');
 require('dotenv').config();
 
 const app = express();
